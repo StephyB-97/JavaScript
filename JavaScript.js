f78a95bc6bbe04d99e25706607ff4223b96dce98
Stephanie Bernades <stephaniebstephy@gmail.com>
2021-09-07 12:11:31 -0500

    Add files via upload
    This code is the JavaScript part of a To-Do list project in which the user can add new tasks to a list, have a view of the list, delete tasks, and quit the program.

--- JavaScript.js
+++ JavaScript.js
@@ -1,58 +1,53 @@
 /***************************************************************************************
  * Name: Stephanie Bernades                                                            *
- * Code purpose: Generate a random number from 0 to the entered number as a limit.     *
- *              Then it is going to ask the user for a number higher or lower to the   * 
- *              random number until it is guessed.                                     *
- *              Guess can quit any time.                                               *
- *              When guessed or quitting it will say the number of attempts made.      *
- *Date: September 5, 2021                                                              */
+ * Code purpose:   To do list that lets add and delete things from a list program      *
+ *Date: September 7, 2021                                                              *
+ ***************************************************************************************/
 
 
 
 //parseInt converts whatever is entered into an integer or NAN
-//promt generates the user for the numbers 
+//promt generates the user for the num
 
 
-//Asks for the maximun number to limit to generate a random number in that range
-let maximum = parseInt(prompt("Enter the maximum number!"));
+//ask the user for a task 
+let input = prompt("What would you like to do?")
 
+//Array that will collect the to dos 
+const todos = [];
 
-//Validate the number entered all the times 
-while (!maximum) {
-    maximum = parseInt(prompt("Enter a valid number!"));
-}
+//Validation loop that ends the programs if quit is entered or continues the program otherwise 
+while (input !== 'quit' && input != 'q') {
 
+    //menu option LIST 
+    if (input === 'list' || input === 'List') {
 
-//Math.floor is going to return the largest integer less than or equal to a given number 
-//this case the number is maximum
-const targetNum = Math.floor(Math.random() * maximum) + 1;
-console.log(targetNum);
+        console.log('*************************************')
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('*************************************')
 
-let guess = parseInt(prompt("Enter your first guess"));
-let attempts = 1;
-
-//Checks if enetered number is equals to generated number
-while (parseInt(guess) != targetNum) {
-
-    //loop to check if the enter value is not q that quits the program 
-    if (guess === 'q')
-        break;
-    attempts++;
-
-    //if the guessed number is greater than the generated random number 
-    if (guess > targetNum) {
-        guess = (prompt("Too high! Enter a new guess:"));
+        //menu option NEW
+    } else if (input === 'new') {
+        const newTodo = prompt('Ok, What is the new to do?');
+        //code to add new elements to the todo array 
+        todos.push(newTodo);
+        //code to display the new task on the list 
+        console.log(`${newTodo} added to the list!`)
     }
-    //if the guessed number is lower than the generated random number 
-    if (guess < targetNum) {
-        guess = (prompt("Too low! Enter a new guess:"));
-    }
-}
 
-//option for when user decides to quit
-if (guess === 'q') {
-    console.log("ok, you quit")
-} else {
-    //message to display when the number is guessed
-    console.log(`YOU GOT IT! It took you ${attempts} guesses`)
+    //menu option DELETE 
+    else if (input == 'delete' || input == 'Delete') {
+        const index = parseInt(prompt('Ok, What to do do you want to delete?:'));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`)
+        } else {
+            console.log('Unknown index')
+        }
+    }
+
+    input = prompt("What would you like to do?")
 }
+console.log("OK QUIT THE APP");
